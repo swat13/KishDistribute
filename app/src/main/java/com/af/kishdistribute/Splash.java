243131e9f0c2b1eada6e87ed27121ccd0e98fb01
swat13 <ali_freidouni@yahoo.com>
2016-09-02 11:21:31 +0400

    Initial commit

---
+++ app/src/main/java/com/af/kishdistribute/Splash.java
@@ -0,0 +1,27 @@
+package com.af.kishdistribute;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+
+
+    }
+}
