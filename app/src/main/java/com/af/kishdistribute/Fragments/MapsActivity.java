59e2e36f6a7c040379f928d11e761ff5a1433f85
swat13 <ali_freidouni@yahoo.com>
2016-09-03 23:14:13 +0400

    70 percent of takhfif Pluss map

---
+++ app/src/main/java/com/af/kishdistribute/Fragments/MapsActivity.java
@@ -0,0 +1,87 @@
+package com.af.kishdistribute.Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.af.kishdistribute.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+    MapView mMapView;
+
+    public static MapsActivity newInstance() {
+        MapsActivity mapsActivity = new MapsActivity();
+        return mapsActivity;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View v = inflater.inflate(R.layout.activity_maps, null);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+
+        mMapView = (MapView) v.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        /*try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                // For showing a move to my location button
+                googleMap.setMyLocationEnabled(true);
+
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(-34, 151);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });*/
+
+        return v;
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
--- app/src/main/java/com/af/kishdistribute/MainActivity.java
+++ app/src/main/java/com/af/kishdistribute/MainActivity.java
@@ -1,25 +1,41 @@
 package com.af.kishdistribute;
 
 import android.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.af.kishdistribute.Fragments.MapsActivity;
 import com.af.kishdistribute.Fragments.OffFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     public OffFragment offFragment;
+    public MapsActivity mapsActivity;
+    private Button bt_off,bt_around,bt_media, bt_profile;
+    FragmentManager fm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fm = getFragmentManager();
+        bt_off = (Button) findViewById(R.id.offButton);
+        bt_around = (Button) findViewById(R.id.aroundButton);
+        bt_media = (Button) findViewById(R.id.mediaButton);
+        bt_profile = (Button) findViewById(R.id.profileButton);
+
+
+        fm = getFragmentManager();
         if (fm != null) {
             offFragment = new OffFragment().newInstance();
             fm.beginTransaction().replace(R.id.detail_fragment, offFragment).addToBackStack(null).commit();
         }
+
+        bt_off.setBackgroundResource(R.drawable.first_sel);
+
 
     }
 
@@ -27,5 +43,43 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.offButton:
+                bt_off.setBackgroundResource(R.drawable.first_sel);
+                bt_around.setBackgroundResource(R.drawable.second);
+                bt_media.setBackgroundResource(R.drawable.third);
+                bt_profile.setBackgroundResource(R.drawable.fourth);
+                break;
+            case R.id.aroundButton:
+                bt_off.setBackgroundResource(R.drawable.first);
+                bt_around.setBackgroundResource(R.drawable.second_sel);
+                bt_media.setBackgroundResource(R.drawable.third);
+                bt_profile.setBackgroundResource(R.drawable.fourth);
+
+                mapsActivity = new MapsActivity().newInstance();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right);
+                ft.add(R.id.detail_fragment, mapsActivity).commit();
+                break;
+            case R.id.mediaButton:
+                bt_off.setBackgroundResource(R.drawable.first);
+                bt_around.setBackgroundResource(R.drawable.second);
+                bt_media.setBackgroundResource(R.drawable.third_sel);
+                bt_profile.setBackgroundResource(R.drawable.fourth);
+                break;
+            case R.id.profileButton:
+                bt_off.setBackgroundResource(R.drawable.first);
+                bt_around.setBackgroundResource(R.drawable.second);
+                bt_media.setBackgroundResource(R.drawable.third);
+                bt_profile.setBackgroundResource(R.drawable.fourth_sel);
+                break;
+            default:
+                break;
+
+        }
     }
 }
